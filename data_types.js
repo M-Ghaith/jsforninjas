592267701b86b3c87d3c4cb80db4f8ab7b059015
Eyas alhumssy <alhumssy@users.noreply.github.com>
2023-02-05 12:20:05 +0300

    Add files via upload

---
+++ data_types.js
@@ -0,0 +1,45 @@
+//task 1 
+let fName = "Eyas";
+console.log(fName);
+//task 2 
+let age = 25;
+console.log(age);
+//task 3 
+let isStudent = true;
+console.log(isStudent);
+//task 4 
+let fruits = ['Apple', 'orange', 'bnane'];
+console.log(fruits);
+
+//task 5 
+let person = {
+   name: 'eyas',
+   age: 29,
+   adress: 'Damascus',
+   email: 'eyas@gmail.com',
+   occupation: 'Web developer'
+}
+console.log(person);
+console.log(person.email);
+
+//task 6
+let w = 10
+let i = 11
+let result = i + w;
+console.log(result);
+
+//task 7 
+const temperature = 80;
+let ishot = (temperature <= 70)
+console.log(ishot);
+
+//task 8 
+function print(text) {
+   console.log(text);
+}
+print("text from function " + fName);
+
+console.log(print("hello"));
+
+
+
---
+++ operators.js
@@ -0,0 +1,57 @@
+//task 1st Arithmetic Operators
+
+let x = 10;
+let y = 16;
+//addition
+let z = (x + y);
+console.log(z);
+
+//subtraction
+let a = (y - x);
+console.log(a);
+
+//multiplication
+let b = (y * x);
+console.log(b);
+
+//division
+let c = (x / y);
+console.log(c);
+
+//remainder i think it rong  
+let d = (x % y);
+console.log(d);
+
+// Task 2nd Comparison Operators
+let m = 30;
+let n = 20;
+
+console.log(m == n);
+console.log(m != n);
+console.log(m > n);
+console.log(m < n);
+console.log(m >= n);
+console.log(m <= n);
+
+//3rd Logical Operators
+
+let age = 29;
+let votingAge= 18;
+
+let canVote = ((age >= votingAge)&&(age > votingAge));
+
+console.log(canVote = "Can vote");
+
+
+let isStudent = true;
+let hasID = false;
+
+let Admittedi = isStudent|| hasID; 
+console.log(Admittedi= "Admittedi");
+
+let notadmitted = isStudent =false|| hasID; 
+console.log(notadmitted = "Not Admittedi");
+
+
+
+
