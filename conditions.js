ec1d02af95da818a292cdd421b9bfeafe2ae5922
Eyas alhumssy <alhumssy@users.noreply.github.com>
2023-02-12 09:43:44 +0300

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,54 @@
+
+//let A = ;
+//console.log(A);
+//let B = (80 >= 89);
+//let C = (70 >= 79);
+//let D = (60 >= 69);
+//let F = (0 >= 59);
+
+let Grade = 98;
+if (Grade >=90 && Grade <=100 ) {
+    console.log("A");
+}
+
+Grade = 88;
+if (Grade >=80 && Grade <=89) {
+    console.log("B");
+}
+Grade = 77;
+if (Grade >=70 && Grade <=79) {
+    console.log("C");
+}
+Grade = 66;
+if (Grade >=60 && Grade <=69) {
+    console.log("B");
+}
+Grade = 3;
+if (Grade >= 0 && Grade <=59) {
+    console.log("F");
+}
+
+//---------------------------//
+//let int;
+//let num;
+//int = 5;
+
+//let sum = (int / 2);
+//console.log(sum);
+
+
+//I cheat sorry
+const number = 70;
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+
+
