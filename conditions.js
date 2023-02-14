49f10727a0d3df06589a45915239be390eb20d7c
Eyas alhomsi <alhomsi.eyas@gmail.com>
2023-02-14 16:34:12 +0300

    fixing last week's mistakes

--- conditions.js
+++ conditions.js
@@ -1,40 +1,27 @@
 
-//let A = ;
-//console.log(A);
-//let B = (80 >= 89);
-//let C = (70 >= 79);
-//let D = (60 >= 69);
-//let F = (0 >= 59);
 
-let Grade = 98;
+let Grade = 40;
 if (Grade >=90 && Grade <=100 ) {
     console.log("A");
+
+}
+else if (Grade >=80 && Grade <=89) {
+    console.log("B");
 }
 
-Grade = 88;
-if (Grade >=80 && Grade <=89) {
-    console.log("B");
-}
-Grade = 77;
-if (Grade >=70 && Grade <=79) {
+else if (Grade >=70 && Grade <=79) {
     console.log("C");
 }
-Grade = 66;
-if (Grade >=60 && Grade <=69) {
+else if
+ (Grade >=60 && Grade <=69) {
     console.log("B");
 }
-Grade = 3;
-if (Grade >= 0 && Grade <=59) {
+
+else if (Grade >= 0 && Grade <=59) {
     console.log("F");
 }
 
-//---------------------------//
-//let int;
-//let num;
-//int = 5;
 
-//let sum = (int / 2);
-//console.log(sum);
 
 
 //I cheat sorry
